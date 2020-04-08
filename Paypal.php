870ba8c7da2cbb4edb715b118a3313a652df1c82
kayzenk <hello@isaac.com.ng>
2020-04-08 10:40:01 +0100

    New updates

---
+++ Paypal.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * PageCarton
+ *
+ * LICENSE
+ *
+ * @category   PageCarton
+ * @package    Paypal_Paypal
+ * @copyright  Copyright (c) 2020 PageCarton (http://www.pagecarton.org)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: Paypal.php Monday 6th of April 2020 05:22PM kayzenk@gmail.com $
+ */
+
+  /** @var Type $var description */
+  public $_version = 1.0;
+
+/**
+ * @see PageCarton_Widget
+ */
+ require_once 'Application/User/Abstract.php';
+class Paypal_Paypal  extends Application_Subscription_Checkout_Abstract_HtmlForm
+{
+	
+	// END OF CLASS
+}
